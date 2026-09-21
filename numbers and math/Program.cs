abb40218aa4f7f8d31bebfce3cb3f158ccb19643
362573354 <362573354@2260I-L29-007>
2026-09-21 12:42:19 -0400

    Add project files.

---
+++ numbers and math/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace numbers_and_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Damien Klassen");
+            Console.WriteLine("Iwas born in " + (2026 - 17));
+            Console.WriteLine( );
+            Console.WriteLine("the average of 14 15 16 is " + (14 + 15 + 16) / 3);
+            Console.WriteLine( );
+            Console.WriteLine("the average of 14 15 16 17 is " + (14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.WriteLine( );
+            Console.WriteLine("the area of a circle witha radius of 5 is " + (5*5*3.14159));
+            Console.WriteLine( );
+            Console.WriteLine("C sharp knows to add when you use the + operator instead of sticking the words together because it will only stick it together if you use the quotation marks before hand");
+        }
+    }
+}
